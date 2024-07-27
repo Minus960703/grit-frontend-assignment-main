fb37341a7622d6c5f27af481a9a273cc4950b30f
JINU <cgzoo9607@gmail.com>
2024-07-27 22:48:10 +0900

    [Fix] Fix Grid Clicked Style & Form Data Render

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,6 +2,7 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import React, { useState } from 'react'
+import { CellClickedEvent, RowClassParams } from 'ag-grid-community';
 
 interface ColumnDefsProps {
   field: string;
@@ -10,14 +11,33 @@ interface ColumnDefsProps {
 interface GridProps {
   columnDefs: ColumnDefsProps[];
   rowData: [];
+  onCellClicked: (parmas: CellClickedEvent) => void;
 }
 
-function Grid({columnDefs, rowData}: GridProps) {
+function Grid({ columnDefs, rowData, onCellClicked }: GridProps) {
+  const [selectedRowIndex, setSelectedRowIndex] = useState<number | null>(null);
+
+  const handleCellClicked = (params: CellClickedEvent) => {
+    setSelectedRowIndex(params.rowIndex);
+    onCellClicked(params);
+  };
+
+
+  const getRowStyle = (params: RowClassParams) => {
+    if (params.node.rowIndex === selectedRowIndex) {
+      return { backgroundColor: '#E5E9F7' };
+    }
+    return null;
+  };
+
   return (
     <div className="ag-theme-alpine" style={{ width: 800, height: 261, margin: '0 auto' }}>
       <AgGridReact
         rowData={rowData}
-        columnDefs={columnDefs}>
+        columnDefs={columnDefs}
+        onCellClicked={handleCellClicked}
+        getRowStyle={getRowStyle}
+      >
       </AgGridReact>
     </div>
   )
