af1d9a6d53b0ed1272e8ac619193b51a701f8488
JINU <cgzoo9607@gmail.com>
2024-07-29 11:10:19 +0900

    [Fix] Fixed Refresh Search Result Maintain

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,20 @@ import { Select } from '@/components'
 import styles from './Header.module.scss'
 import { useNavigate } from 'react-router-dom'
 
-function Header({ searchParam, isChangeSearchParamSelectActive, isChangeSelectBoxItems }) {
+function Header({
+  searchParam,
+  isChangeSearchParamSelectActive,
+  isChangeSelectBoxItems,
+  updateFormCheck
+}) {
   const navigate = useNavigate();
   const moveToPrevPage = () => {
-    navigate('/');
+    if (updateFormCheck) {
+      const userConfirmed = window.confirm('수정중인 내용이 있습니다.\n 지금 뒤로가시면 저장되지 않습니다.\n 뒤로 가시겠습니까?');
+      if (userConfirmed) {
+        navigate('/');
+      }
+    } else navigate('/');
   }
   return (
     <header className={styles.header}>
